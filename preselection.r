0289b7ac79e48f05a52b666f9292bec977e970ef
emawithonem <palsinaaer@gmail.com>
2021-10-01 16:35:19 +0200

    added filter flowchart & start ATC
    The first filter for PERSONS is now working nicely, with 2 outputs, the filtrations flowchart showing attrition and the case IDs of elligible cases that can be applied to the other data tables.
    
    Started ATC selection, having some weird problems creating dummy data.

--- preselection.r
+++ preselection.r
@@ -23,16 +23,25 @@ rm(gender)
 rm(numloc)
 colnames(mydata)
 
-#firstflter function selects females within age range from the persons table and stores the selected IDs
+#firstflter function selects females within age range from the persons table and stores the selected IDs to use for subsequent filtering
 
-firstfilter<-function(personstable=PERSONS, caseid="person_id", sex="sex_at_instance_creation", female=0, dob= "year_of_birth", dobmin=1954, dobmax=2008) {
+firstfilter<-function(personstable=PERSONS, caseid="person_id", sex="sex_at_instance_creation", female="F", dob= "year_of_birth", dobmin=1954, dobmax=2008) {
 newdata<-personstable[(personstable[,sex]==female),]
+flowchart_gender<-c(nrow(personstable), nrow(newdata))
 newdata<-newdata[(newdata[,dob]>=dobmin),]
 filtered_data<-newdata[(newdata[,dob]<=dobmax),]
-  return(filtered_data)
+flowchart_age<-nrow(filtered_data)
+flowchart_steps<-c("original", "females only", "1954<=DOB<=2008")
+filter_ID<-(filtered_data[[caseid]])
+flowchart_filter<-as.data.frame(cbind(flowchart_steps, c(flowchart_gender, flowchart_age)))
+filter_output<-list(filter_ID, flowchart_filter)
+  return(filter_output)
 }
 
 example_filtered_data<-firstfilter(personstable=mydata, caseid="numloc", sex="gender", female=0, dob = "datebirth", dobmin = 1954, dobmax = 2008)
+example_filtered_data
+
+plot(example_filtered_data$flowchart_steps, example_filtered_data$V2, lwd=3)
 
 #check on sample data
 
@@ -56,16 +65,36 @@ summary(filtered_data)
 
 #simulate MEDICINES table
 
-ATC1<-sample(c("A10AE01", "P16AE01", "B20AE01", "A37GE01", "N10AE01"),100, replace = T)
-ATC2<-sample(c("A10AE01", "P16AE01", "B20AE01", "A37GE01", "N10AE01"),100, replace = T)
-ATC3<-sample(c("A10AE01", "P16AE01", "B20AE01", "A37GE01", "N10AE01"),100, replace = T)
+#only 1 column for ATC codes (nice)
 
-MEDICINES<-as.data.frame(cbind(numloc, ATC1, ATC2, ATC3))
+
+#isolate first 4 cijfers
+substr(MEDICINES$ATC1, 1, 4)
+
+#first step, establish ATC library
+
+#mix of SAP ATCs and random ATCs
+# random
+ATC1<-sample(c("A10AE01", "P16AE01", "B20AE01", "A37GE01", "N10AE01"),100, replace = T)
+# SAP
+ATC_lib<-as.data.frame(read.csv("C:\\Users\\Acer\\OneDrive\\Documents\\work\\lot4\\ATC_lot4.csv"))
+colnames(ATC_lib)<-c("drug_name", "ATC")
+ATC_lib<-unique(ATC_lib)
+ATC_lib<-ATC_lib[complete.cases(ATC_lib)==T,]
+ATC_lot4<-ATC_lib$ATC
+
+#something going wrong here concatenate causing integers 
+
+ATC<-(c(ATC1[1:50], ATC_lot4[1:50]))
+numloc<-(1:100)
+MEDICINES<-as.data.frame(cbind(numloc, ATC))
 
 rm(numloc)
 rm(ATC1)
-rm(ATC2)
-rm(ATC3)
+rm(ATC)
+#second match to data
 
-#first step, match to filtered data
+MEDICINES[MEDICINES[,"ATC"]
+          
+          %in%ATC_lib$ATC]
 
