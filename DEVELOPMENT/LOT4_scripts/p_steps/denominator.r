6684da7402ed2c26a6b1be2299a82b58451d0c6a
rpajou <rpajou@users.noreply.github.com>
2021-11-25 15:12:33 +0100

    Create denominator.r

---
+++ DEVELOPMENT/LOT4_scripts/p_steps/denominator.r
@@ -0,0 +1,57 @@
+#this script counts the number of active/elligible participants per month in the study (denominator for events)
+
+if(!require(rstudioapi)){install.packages("rstudioapi")}
+library(rstudioapi)
+if(!require(rstudioapi)){install.packages("lubridate")}
+library(lubridate)
+if(!require(rstudioapi)){install.packages("zoo")}
+library(zoo)
+
+
+
+projectFolder<-dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(projectFolder)
+setwd('..')
+base_folder<-getwd()
+
+
+readRDS(paste0(base_folder, "/g_intermediate/populations/ALL_study_population.rds"))
+
+start.date<-as.Date(ALL_study_population$start_follow_up)
+end.date<-as.Date(ALL_study_population$end_follow_up)
+
+
+ym1 <- as.yearmon(as.character(start.date), "%Y-%m-%d") # convert to yearmon
+ym2 <- as.yearmon(as.character(end.date), "%Y-%m-%d") # ditto
+
+#get the seq of months between start dates and end dates in a list 
+#easy with a for loop, but better with apply 
+
+#start with a loop and then see how slow it is, make more elegant later
+
+FUmonths<-list()
+
+for (i in 1:length(ym1)){
+  s <- seq(ym1[i], ym2[i], (1/12)) # create yearmon sequence
+  s<-as.numeric(format(s, "%Y%m"))
+  FUmonths[[i]]<-s
+}
+
+#doesn't take too long
+
+FUmonths<-unlist(FUmonths)
+studyFUmonths<-FUmonths[(FUmonths>=200901)&(FUmonths<=201912)]
+
+FUmonths_df<-as.data.frame(table(studyFUmonths))
+FUmonths_df$YM<-as.Date(paste0(as.character(FUmonths_df$studyFUmonths),"01"), format="%Y%m%d")
+FUmonths_df$YM<-format(as.Date(FUmonths_df$YM),"%Y-%m")
+
+# for plots need to split(FUmonths,by=year)?
+
+saveRDS(data.table(FUmonths_df), paste0(base_folder, "/g_intermediate/populations/denominator.rds"))
+write.csv(FUmonths_df,paste0(base_folder, "/g_intermediate/populations/denominator.csv"))
+
+pdf((paste0(base_folder, "/g_output/plots/denominator.pdf")), width=8, height=4)
+plot(FUmonths_df$studyFUmonths, FUmonths_df$Freq, ylab="Persons Observed per Month", xlab="Year and Month")
+dev.off()
+
