0d65967a9b906f1f481acf8cd73c2aac36ee3e5a
madziag <gamba_m@yahoo.com>
2021-11-26 15:32:33 +0100

    denominator file sourced before monthly counts
    Denominator file is needed to calculate monthly rates in monthly counts

--- DEVELOPMENT/LOT4_scripts/to_run_source_population_counts.r
+++ DEVELOPMENT/LOT4_scripts/to_run_source_population_counts.r
@@ -4,11 +4,28 @@ library(rstudioapi)
 
 projectFolder<-dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(projectFolder)
-#######################################################################################################
-######################################################################################################
-#set to TRUE to clear out intermediate files PLEASE REPLACE T WITH F IF YOU WANT TO SAVE INTERMEDIATE DATA SETS, I.E. TO REDUCE AMOUNT OF STORED DATA”
-clear_int_files<-T
-################################################
+########################################################################################################################
+#Specify all meanings that refer to birth registry in the SURVEY_ID table(if applicable) for identifying pregnancy
+########################################################################################################################
+#example meanings_birth_registry<-c("birth_registry", "birth_registry_meanings")
+#keep in mind this records will be classified as end_of_pregnancy so no spontaneous abortion registry should be included
+# meanings_birth_registry<-c("birth_registry_mother")
+########################################################################################################################
+#Specify all variables of interest to generate the Lifestyle report
+########################################################################################################################
+#Variables of interest:Smoking, Folic acid use, Alcohol abuse, BMI, SES
+#1.Identify the CDM table you used to save the information about the variables of interest.
+#2.Identify the original name of the variable of interest.
+#3.Use the information above to complete the list below.
+#4.CDM_table:name of the CDM table where you saved the information.
+#5.CDM_column: name of the CDM column where you saved the information about the name of the variable of interest.
+#6.value: name of the original variable.
+#7.c.voc: name of the CDM column where you saved the vocabulary representing the variable of interest.If no vocabulary fill NULL
+#8.v.voc: the vocabulary used for the variable of interest.If no vocabulary fill NULL
+#9.v.date: name of the CDM column which saves the date of recording.
+#10.If you don't have information about a variable then delete that section and use Lifestyle <- list()
+
+###############################################
 
 source("99_path.R")
 source(paste0(pre_dir,"packages.R"))
@@ -20,9 +37,10 @@ setwd(projectFolder)
 #Study_source_population
 #################################################
 source(paste0(pre_dir,"study_source_population_script.R"))
+source(paste0(pre_dir,"denominator.r"))
 source(paste0(pre_dir,"monthly_counts_dxcodes.R"))
 source(paste0(pre_dir,"monthly_counts_ATC.R"))
-source(paste0(pre_dir, "denominator.r"))
+
 ##############################################
 #plot output
 #############################################
@@ -30,12 +48,5 @@ source(paste0(pre_dir, "denominator.r"))
 mask<-T
 
 #to see unmasked plots, set mask<-F
-
 source(paste0(pre_dir,"LOT4plots.r"))
 
-############################
-#CLEAN UP
-if(clear_int_files==T){
-unlink(paste0(g_intermediate, "/tmp"), recursive = TRUE)
-unlink(paste0(g_intermediate, "/populations"), recursive = TRUE)  
-}
