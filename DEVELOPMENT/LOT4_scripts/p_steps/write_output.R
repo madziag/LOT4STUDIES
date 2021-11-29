a4bc1a4e84cc7523e11b028a9942f31e4993fe85
emawithonem <palsinaaer@gmail.com>
2021-11-29 17:34:09 +0100

    Update write_output.R
    debug [1]-->[i]

--- DEVELOPMENT/LOT4_scripts/p_steps/write_output.R
+++ DEVELOPMENT/LOT4_scripts/p_steps/write_output.R
@@ -6,11 +6,11 @@ if(!require(xlxs)){install.packages("xlxs")}
 library(xlxs)}
 
 all_rds_outputs<-list.files(output_dir, recursive=T, pattern="rds")
-dir.create(paste0(output_dir,"/",my_format, "_files"))
+dir.create(paste0(output_dir,my_format, "_files"))
 my_output_folder<-paste0(output_dir,my_format, "_files/")
 
 for (i in 1:length(all_rds_outputs)){
-  my_table<-readRDS(paste0(output_dir, all_rds_outputs[1]))
+  my_table<-readRDS(paste0(output_dir, all_rds_outputs[i]))
   if(my_format=="csv"){write.csv(my_table, my_output_folder)}
   else{writexl::write_xlsx(mytable,my_output_folder )}
 }
