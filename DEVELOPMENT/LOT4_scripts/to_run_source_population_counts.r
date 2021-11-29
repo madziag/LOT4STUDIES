9d8de54ed91666974033e5a4145f0625582d2585
rpajou <rpajou@users.noreply.github.com>
2021-11-29 12:47:32 +0100

    Update to_run_source_population_counts.r
    removed redundant comments from to_run file

--- DEVELOPMENT/LOT4_scripts/to_run_source_population_counts.r
+++ DEVELOPMENT/LOT4_scripts/to_run_source_population_counts.r
@@ -4,26 +4,6 @@ library(rstudioapi)
 
 projectFolder<-dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(projectFolder)
-########################################################################################################################
-#Specify all meanings that refer to birth registry in the SURVEY_ID table(if applicable) for identifying pregnancy
-########################################################################################################################
-#example meanings_birth_registry<-c("birth_registry", "birth_registry_meanings")
-#keep in mind this records will be classified as end_of_pregnancy so no spontaneous abortion registry should be included
-# meanings_birth_registry<-c("birth_registry_mother")
-########################################################################################################################
-#Specify all variables of interest to generate the Lifestyle report
-########################################################################################################################
-#Variables of interest:Smoking, Folic acid use, Alcohol abuse, BMI, SES
-#1.Identify the CDM table you used to save the information about the variables of interest.
-#2.Identify the original name of the variable of interest.
-#3.Use the information above to complete the list below.
-#4.CDM_table:name of the CDM table where you saved the information.
-#5.CDM_column: name of the CDM column where you saved the information about the name of the variable of interest.
-#6.value: name of the original variable.
-#7.c.voc: name of the CDM column where you saved the vocabulary representing the variable of interest.If no vocabulary fill NULL
-#8.v.voc: the vocabulary used for the variable of interest.If no vocabulary fill NULL
-#9.v.date: name of the CDM column which saves the date of recording.
-#10.If you don't have information about a variable then delete that section and use Lifestyle <- list()
 
 ###############################################
 
