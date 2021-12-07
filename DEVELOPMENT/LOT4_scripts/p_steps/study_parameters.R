076d6e37817a4078df59852369e1d4ebd165d41d
Ema <33512065+emawithonem@users.noreply.github.com>
2021-12-07 14:39:15 +0100

    Update study_parameters.R
    fixed study start date and min age

--- DEVELOPMENT/LOT4_scripts/p_steps/study_parameters.R
+++ DEVELOPMENT/LOT4_scripts/p_steps/study_parameters.R
@@ -6,11 +6,9 @@ sex<-"F"
 min_age_preg<-12
 max_age_preg<-55
 #Set parameters basic parameters
-start_study_date <- "19950101"
+start_study_date <- "20100101"
 end_study_date <- "20211231"
-#start_study_date <- "20140101"
-#end_study_date <- "20181231"
 lookback_period <- 365
-Age_max <- 56
-Age_min <- 0
+Age_max <- 55
+Age_min <- 12
 
