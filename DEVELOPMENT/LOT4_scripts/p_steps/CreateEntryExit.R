fc7e6cc4c13af86c9d4b8dc5a4e8aa985cd30c36
emawithonem <palsinaaer@gmail.com>
2021-12-10 14:54:38 +0100

    Update CreateEntryExit.R
    ALL_study_population merged only sterilization_date, and then entry and exit added
    then data saved (overwrite original file)

--- DEVELOPMENT/LOT4_scripts/p_steps/CreateEntryExit.R
+++ DEVELOPMENT/LOT4_scripts/p_steps/CreateEntryExit.R
@@ -6,7 +6,7 @@ ALL_study_population<-readRDS(paste0(populations_dir, "ALL_study_population.rds"
 start_date<-as.Date(as.character(20100101), "%Y%m%d")
 
 ALL_study_population$entry_date<-as.Date(as.character("18000101"), "%Y%m%d")
-
+print("calculating entry date")
 for(i in 1:nrow(ALL_study_population)){
   entry_dates<-c(ALL_study_population$date_min[i], ALL_study_population$op_start_date[i], start_date)
   max_date<-max(entry_dates)
@@ -14,14 +14,16 @@ for(i in 1:nrow(ALL_study_population)){
 }
 
 summary(ALL_study_population$entry_date)
+print("entry date OK")
 
 #exit date: earliest date at which ANY of the following conditions are met: age>55, observation ends, study ends, sterilization record
 # using ALL_study_populations AND sterilization
 sterility<-readRDS(paste0(g_intermediate,"tmp/sterility/sterility_all_first_occurrence.rds"))
+sterility<-sterility[,c("person_id", "sterility_date")]
 ALL_study_population<-dplyr::left_join(ALL_study_population, sterility)
 end_date<-as.Date(as.character(20201231), "%Y%m%d")
 
-
+print("calculating exit date")
 for(i in 1:nrow(ALL_study_population)){
   exit<-c(ALL_study_population$date_max[i], ALL_study_population$op_end_date[i], ALL_study_population$sterility_date[i], end_date)
   min_date<-min(exit,na.rm = T)
@@ -29,5 +31,6 @@ for(i in 1:nrow(ALL_study_population)){
 }
 
 summary(ALL_study_population$exit_date)
-
+print("exit date OK")
+saveRDS(ALL_study_population, paste0(populations_dir, "ALL_study_population.rds"))
 
