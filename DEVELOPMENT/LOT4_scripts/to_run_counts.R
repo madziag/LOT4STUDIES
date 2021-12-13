f4c1f805794f7a1b962fe57da6baf346dfac2f90
emawithonem <palsinaaer@gmail.com>
2021-12-13 14:24:34 +0100

    Update to_run_counts.R
    have paths in environment without overwriting folders

--- DEVELOPMENT/LOT4_scripts/to_run_counts.R
+++ DEVELOPMENT/LOT4_scripts/to_run_counts.R
@@ -9,7 +9,7 @@ setwd(projectFolder)
 ### NOTE FOR DAPS: If you have run the preselection script and would like to use the subsetted data sets that it produces instead of your full ETL'd data files, you need to go to the "99_path.R" file and choose the second path option, by adding a "#" symbol at the start of line 7, and removing the "#" symbol at the start of line 8. If the preselection files have been stored elsewhere, then the path will need to be set manually.
 
 ### Below you must set
-source("99_path.R")
+source("paths_shortcut.R")
 source(paste0(pre_dir,"packages.R"))
 source(paste0(pre_dir,"info.R"))
 source(paste0(pre_dir,"study_parameters.R"))
