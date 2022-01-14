1d6851c68394960ebf2bd7b68caeba5ca4557026
madziag <gamba_m@yahoo.com>
2022-01-14 10:27:22 +0100

    Removal of testing code + check if there are any records that meet the retinoid/valproate use during pregnancy

--- DEVELOPMENT/LOT4_scripts/p_steps/monthly_counts_med_use_in_pregnancy.R
+++ DEVELOPMENT/LOT4_scripts/p_steps/monthly_counts_med_use_in_pregnancy.R
@@ -19,46 +19,44 @@ study_pop_meds[,person_id:=as.character(person_id)]
 study_pop_meds[,event_date:=as.IDate(event_date,"%Y%m%d")]
 # Merge with pregnancy list to get list of patients who were pregnant and used Retinoids/Valproates
 study_pop_meds <- D3_pregnancy_reconciled[study_pop_meds, on="person_id", nomatch=0]
-# Testing 
-study_pop_meds[study_pop_meds$person_id == "ConCDM_SIM_200421_00331" & study_pop_meds$event_date == "2012-06-05",]$event_date<- as.Date("2010-12-06")
-study_pop_meds[study_pop_meds$person_id == "ConCDM_SIM_200421_00925" & study_pop_meds$event_date== "2019-05-08",]$event_date<- as.Date("2013-07-13")
-study_pop_meds[study_pop_meds$person_id == "ConCDM_SIM_200421_00247" & study_pop_meds$event_date== "2012-03-12",]$event_date<- as.Date("2014-07-28")
 # Remove records where medication was prescribed/dispensed outside of the pregnancy period
 study_pop_meds <- study_pop_meds[study_pop_meds$event_date >= study_pop_meds$pregnancy_start_date & study_pop_meds$event_date <= study_pop_meds$pregnancy_end_date,]
-# Get unique values of med_type column 
-med_type_unique <- unique(study_pop_meds$med_type)
-# Monthly Counts
-if (length(med_type_unique > 0)){
-  for (mt in 1:length(med_type_unique)){
-    # print(med_type_unique[mt])
-    med_df1 <- study_pop_meds[study_pop_meds$med_type == med_type_unique[mt],]
-    # # Perform counts per month/year for every available highest_quality level
-    # # Get unique values of column "highest_quality"
-    hq_unique <- unique(med_df1$highest_quality)
-    
-    if (length(hq_unique) > 0){
-      for (hq in 1:length(hq_unique)){
-        # print(hq_unique[hq])
-        med_df <- med_df1[med_df1$highest_quality == hq_unique[hq],]
-        if (nrow(med_df) >0){
-          counts <- med_df[,.N, by = .(year(med_df$event_date),month(med_df$event_date))]
-          counts <- as.data.table(merge(x = empty_counts, y = counts, by = c("year", "month"), all.x = TRUE))
-          counts[is.na(counts[,N]), N:=0]
-          # Calculate rates
-          counts <- within(counts, YM<- sprintf("%d-%02d", year, month))
-          counts <- merge(x = counts, y = FUmonths_df, by = c("YM"), all.x = TRUE)
-          counts <-counts[,c("YM", "N", "Freq")]
-          counts <-counts[,rates:=as.numeric(N)/as.numeric(Freq)]
-          print(paste("Saving monthly counts: Use of", med_type_unique[mt], "during pregnancy, data quality level:", hq_unique[hq]))
-        # Save file
-        if(SUBP == TRUE){
-          pop_names <- gsub(".rds", "", populations[pop])
-          saveRDS(counts, paste0(preg_med_counts,"/",pop_names, "_", hq_unique[hq], "_preg_", med_type_unique[mt], "_counts.rds"))
-        }else{
-          saveRDS(counts, paste0(preg_med_counts,"/", med_type_unique[mt], "_preg_",hq_unique[hq] , "_counts.rds"))
+
+if(nrow(study_pop_meds) > 0) {
+  # Get unique values of med_type column 
+  med_type_unique <- unique(study_pop_meds$med_type)
+  # Monthly Counts
+  if (length(med_type_unique > 0)){
+    for (mt in 1:length(med_type_unique)){
+      med_df1 <- study_pop_meds[study_pop_meds$med_type == med_type_unique[mt],]
+      # # Perform counts per month/year for every available highest_quality level
+      # # Get unique values of column "highest_quality"
+      hq_unique <- unique(med_df1$highest_quality)
+      if (length(hq_unique) > 0){
+        for (hq in 1:length(hq_unique)){
+          med_df <- med_df1[med_df1$highest_quality == hq_unique[hq],]
+          if (nrow(med_df) >0){
+            counts <- med_df[,.N, by = .(year(med_df$event_date),month(med_df$event_date))]
+            counts <- as.data.table(merge(x = empty_counts, y = counts, by = c("year", "month"), all.x = TRUE))
+            counts[is.na(counts[,N]), N:=0]
+            # Calculate rates
+            counts <- within(counts, YM<- sprintf("%d-%02d", year, month))
+            counts <- merge(x = counts, y = FUmonths_df, by = c("YM"), all.x = TRUE)
+            counts <-counts[,c("YM", "N", "Freq")]
+            counts <-counts[,rates:=as.numeric(N)/as.numeric(Freq)]
+            print(paste("Saving monthly counts: Use of", med_type_unique[mt], "during pregnancy, data quality level:", hq_unique[hq]))
+            # Save file
+            if(SUBP == TRUE){
+              pop_names <- gsub(".rds", "", populations[pop])
+              saveRDS(counts, paste0(preg_med_counts,"/",pop_names, "_", hq_unique[hq], "_preg_", med_type_unique[mt], "_counts.rds"))
+            }else{
+              saveRDS(counts, paste0(preg_med_counts,"/", med_type_unique[mt], "_preg_",hq_unique[hq] , "_counts.rds"))
+            }
+          }
         }
-      }
       }
     }
   }
+} else {
+  print(paste("There are", nrow(study_pop_meds), "patient(s) who used Retinoids/Valproates during their pregnancy" ))
 }
