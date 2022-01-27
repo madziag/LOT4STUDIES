706a2385603c99d6fd34ca39a48161003aa70c84
emawithonem <palsinaaer@gmail.com>
2022-01-27 14:19:08 +0100

    Create plot_age_distribution.R

---
+++ DEVELOPMENT/LOT4_scripts/p_steps/plot_age_distribution.R
@@ -0,0 +1,26 @@
+#boxplots per year of age of study pop
+
+if(!require(lubridate)){install.packages("lubridate")}
+library(lubridate)
+if(!require(dplyr)){install.packages("dplyr")}
+library(dplyr)
+
+# load study population with entry and exit dates
+mydata<-readRDS(paste0(populations_dir,list.files(populations_dir)))
+# dataframe to store ids
+myplotdata<-data.frame(matrix(ncol=length(seq(2010,2020, by=1))))
+colnames(myplotdata)<-as.character(seq(2010,2020, by=1))
+myyears<-(seq(2010,2020, by=1))
+
+#assign personIDs to all years of participation
+myentry<-lubridate::year(mydata$entry_date)
+myexit<-lubridate::year(mydata$exit_date)
+
+for(i in 1:ncol(myplotdata)){
+  for(j in 1:nrow(mydata)){
+    if(between(x=myyears[i], left=myentry[j], right=myexit[j])==T){myplotdata[j,i]<-(2020-mydata$year_of_birth[j])}else{myplotdata[j,i]<-NA}
+  }
+}
+
+summary(myplotdata)
+boxplot(myplotdata, main="age distribution of participants by year")
